db548a522d35662427c978c8f2294c9f1fc9c7a7
Sureshkumar T <sureshkumar.thangavel@walmartlabs.com>
2020-04-28 06:38:31 +0530

    add custom material ui theme

--- src/index.js
+++ src/index.js
@@ -5,14 +5,30 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { myCreateStore } from "./mystore";
 import "typeface-roboto";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: "#fff",
+            main: "#76bd8e",
+            dark: "#000"
+        },
+        secondary: {
+            main: "#f44336"
+        }
+    }
+});
 
 const store = myCreateStore();
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <MuiThemeProvider theme={theme}>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </MuiThemeProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
